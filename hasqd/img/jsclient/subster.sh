933814429da8e9f93984c07a03970027cea9789d
underr <underr-ua@users.noreply.github.com>
2015-12-25 13:32:45 +0200

    jsclient fix

---
+++ hasqd/img/jsclient/subster.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+error() {
+    [ -z "$1" ] || echo "$1" >&2
+    exit 1
+}
+
+while read -r LINE; do
+    if [ "$LINE" = "INCLUDEFILE" ]; then
+        read LINE
+        [ -r "$LINE" ] || error "cant open file $LINE"
+            while read -r SLINE; do
+                echo "r+='$SLINE\n';"
+            done < "$LINE"
+        continue
+    fi
+    echo "$LINE"
+done
