bc5a91cb3cd3a9d21eca2b0bc3bc9f10c25904d9
mazonka <mazonka@users.noreply.github.com>
2016-03-25 17:36:56 +1030

    style 7

--- hasqd/img/jsclient/eng/sh_hash_sha512.js
+++ hasqd/img/jsclient/eng/sh_hash_sha512.js
@@ -228,7 +228,7 @@ function rstr2binb(input)
         output[i] = 0;
     for (var i = 0; i < input.length * 8; i += 8)
         output[i >> 5] |= (input.charCodeAt(i / 8) & 0xFF) << (24 - i % 32);
-    return output;
+        return output;
 }
 
 /*
--- hasqd/img/jsclient/stylejs.sh
+++ hasqd/img/jsclient/stylejs.sh
@@ -11,11 +11,16 @@ sed 's/case.*\:/\0\`;/g' $fl > $fl.1.tmp
 sed 's/\:/\`/g' $fl.1.tmp > $fl.2.tmp
 sed 's/===/\`~~/g' $fl.2.tmp > $fl.3.tmp
 sed 's/\!==/\`\`~/g' $fl.3.tmp > $fl.4.tmp
-sed 's/*.\/*./\/\/\`\0/g' $fl.4.tmp > $fl.8.tmp
 
-cat $fl.8.tmp > $fl.x.tmp
-cmd /c style $fl.8.tmp
-cat $fl.8.tmp > $fl.5.tmp
+sed 's/\/\*/\`\*1/g' $fl.4.tmp > $fl.5.tmp
+sed 's/\*\//\`\*2/g' $fl.5.tmp > $fl.6.tmp
+sed 's/.*\/.*/\/\/\`\0/g' $fl.6.tmp > $fl.7.tmp
+sed 's/`\*1/\/\*/g' $fl.7.tmp > $fl.8.tmp
+sed 's/`\*2/\*\//g' $fl.8.tmp > $fl.y.tmp
+
+cat $fl.y.tmp > $fl.x.tmp
+cmd /c style $fl.y.tmp
+cat $fl.y.tmp > $fl.5.tmp
 
 sed 's/\/\/`//g' $fl.5.tmp > $fl.4.tmp
 sed 's/``~/!==/g' $fl.4.tmp > $fl.3.tmp
