bc8fffb5fa784491313e067e2a3d6f0afc8e095a
mazonka <mazonka@users.noreply.github.com>
2016-03-25 13:43:06 +1030

    style 2

--- hasqd/img/jsclient/eng/sh_hash_smd.js
+++ hasqd/img/jsclient/eng/sh_hash_smd.js
@@ -1,10 +1,13 @@
 // Hasq Technology Pty Ltd (C) 2013-2016
 
-function hex_smd(data){
-	if (data !== undefined && data !== null) {
-		if (data.length !== 0) {
-			return hex_md5(hex_sha256(data));
-		}
-	}
-	return '';
+function hex_smd(data)
+{
+    if (data !== undefined && data !== null)
+    {
+        if (data.length !== 0)
+        {
+            return hex_md5(hex_sha256(data));
+        }
+    }
+    return '';
 }
