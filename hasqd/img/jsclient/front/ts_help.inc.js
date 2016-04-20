005d0381e869cc09c255456e58c87d9c5be835fa
underr <underr-ua@users.noreply.github.com>
2016-04-20 07:08:30 +0300

    jsclnts

--- hasqd/img/jsclient/front/ts_help.inc.js
+++ hasqd/img/jsclient/front/ts_help.inc.js
@@ -10,12 +10,12 @@ function gHelp(x)
 ///        token_name: 'FIXME (ts_message.js)<br\>token_name',
 ///        token_hash: 'FIXME (ts_message.js)<br\>token_hash',
 ///        master_key: 'FIXME (ts_message.js)<br\>master_key',
-        welcome_to_tokenswap: 'FIXME (ts_message.js)<br\>welcome_to_tokenswap',
-        token_data: 'FIXME (ts_message.js)<br\>token_data',
-        create_new_token: 'FIXME (ts_message.js)<br\>create_new_token',
-        show_keys: 'FIXME (ts_message.js)<br\>show_keys',
-        paste_keys: 'FIXME (ts_message.js)<br\>paste_keys',
-        search_for_tokens: 'FIXME (ts_message.js)<br\>search_for_tokens'
+///         welcome_to_tokenswap: 'FIXME (ts_message.js)<br\>welcome_to_tokenswap',
+///			token_data: 'FIXME (ts_message.js)<br\>token_data',
+///			create_new_token: 'FIXME (ts_message.js)<br\>create_new_token',
+///			show_keys: 'FIXME (ts_message.js)<br\>show_keys',
+///			paste_keys: 'FIXME (ts_message.js)<br\>paste_keys',
+///			search_for_tokens: 'FIXME (ts_message.js)<br\>search_for_tokens'
     };
 
     if (o[x]) return o[x];
@@ -52,6 +52,35 @@ function gHelp(x)
         INCLUDEFILE
         txt/ts_master_key.htm
     }
-
+    else if (x == 'welcome_to_tokenswap')
+    {
+        INCLUDEFILE
+        txt/ts_welcome_to_tokenswap.htm
+    }
+    else if (x == 'token_data')
+    {
+        INCLUDEFILE
+        txt/ts_token_data.htm
+    }    
+	else if (x == 'create_new_token')
+    {
+        INCLUDEFILE
+        txt/ts_create_new_token.htm
+    }	
+	else if (x == 'show_keys')
+    {
+        INCLUDEFILE
+        txt/ts_show_keys.htm
+    }
+	else if (x == 'paste_keys')
+    {
+        INCLUDEFILE
+        txt/ts_paste_keys.htm
+    }		
+	else if (x == 'search_for_tokens')
+    {
+        INCLUDEFILE
+        txt/ts_search_for_tokens.htm
+    }		
     return r;
 }
