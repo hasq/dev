f90517e763aa147e27925a179f1350fb54b88d5b
mazonka <mazonka@users.noreply.github.com>
2016-04-24 13:56:06 +0930

    moving tools

--- admin/worksheet/run_timeapp.bat
+++ admin/worksheet/run_timeapp.bat
@@ -1,20 +1,22 @@
 
 @if NOT EXIST hide.exe (
-	@echo Please compile hide.cpp
+	@echo Please compile hidee.cpp
+	@echo e.g. g++ ../tools/hidee.cpp -o hidee.exe
 	@exit
 )
 
 @if NOT EXIST timeapp.exe (
 	@echo Please compile timeapp.cpp
+	@echo e.g. g++ ../tools/timeapp.cpp -o timeapp.exe
 	@exit
 )
 
 hide
 svn up timesheet.e
-hide timesheet.e
-hide tech_tasks.txt.e
+hidee timesheet.e
+hidee tech_tasks.txt.e
 timeapp.exe
-hide timesheet
+hidee timesheet
 svn ci timesheet.e -m "time"
 del tech_tasks.txt
 move timesheet ts_backup.tmp
