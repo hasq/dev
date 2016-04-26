88551eade38cfd4eeb5f57347de452a89b960e76
underr <underr-ua@users.noreply.github.com>
2016-04-26 16:44:04 +0300

    mobile css

--- hasqd/img/jsclient/front/ts_html_base.js
+++ hasqd/img/jsclient/front/ts_html_base.js
@@ -181,14 +181,14 @@ function widGetHTMLTokenHash()
     var body = function()
     {
         var r = '';
-        r += '<td id="td_reload_button" class="td-info" title="' + gTooltip.refresh_token + '"/>\n';
+        r += '<td id="td_reload_button" title="' + gTooltip.refresh_token + '"/>\n';
         r += widGetHTMLSpanImg('span_reload', 'widReloadTokenInfo(null, 0, true)');
         r += '<td id="td_token_hash_label" class="td-info"/>\n';
         r += widGetHTMLMessageBox('Token hash:');
         r += '<td id="td_token_hash" class="td-info"/>\n';
-        r += '<td id="td_token_pic" class="td-info"/>\n';
+        r += '<td id="td_token_pic"/>\n';
         r += widGetHTMLMessageBox(widGetHTMLSpanImg('span_token_pic'));
-        r += '<td id="td_file_upload" class="td-info"/>\n';
+        r += '<td id="td_file_upload"/>\n';
         r += '<label id="label_file_upload" for="input_file_upload">File</label>\n';
         r += '<input id="input_file_upload" type="file" onchange="return widLoadFiles(this.files)"></input>';
         return r;
