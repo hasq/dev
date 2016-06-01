c14d0a3c98c57bcc9e3c7dfef821c65f9888ddf3
underr <underr-ua@users.noreply.github.com>
2016-06-01 07:04:15 +0300

    hqclnt message file

---
+++ hasqd/img/jsclient/front/hq_message.js
@@ -0,0 +1,33 @@
+var gButton = 
+{
+	continueBtn: 'Continue',
+	hiddenBtn: 'Hidden'
+};
+
+var gMsg =
+{
+	cancel: 'Operation cancelled!',
+	userWait: 'Waiting for user decision...',
+	emptyName: 'Empty basename.',
+	badTokens: 'Empty or bad tokens.',
+	emptyPassword: 'Empty password.',
+	incorrectName: 'Incorrect basename - please, remove spaces.',
+	incorrectIdx: 'Incorrect indexes - the start index should not be bigger the end index',
+	manyTokens: 'Too many tokens, the maximum quantity - 128',
+	namePlaceholder: 'Please enter a hashes or raw value in square brackets',
+	passwordPlaceHolder: 'Please enter a password',
+	charsLeft: 'Warning! Chars left: ',
+	wait: 'Please wait...',
+	error: 'Error occured: ',
+	unavailable: 'Unavailable tokens presents!',
+	ok: 'OK'
+};
+
+var gTokStateMsg = 
+{
+	ok: 'OK',
+	sending: 'Token is locked by sending',
+	receiving: 'Token is locked by receiving',
+	wrong: 'Wrong password',
+	nodn: 'No such token'
+}
