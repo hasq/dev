0dd349aa1b5d5927fec720126fc283ef635089f1
underr <underr-ua@users.noreply.github.com>
2016-08-26 06:59:29 +0300

    makeiso

--- hasqd/img/maketciso.sh
+++ hasqd/img/maketciso.sh
@@ -1,87 +1,44 @@
 #!/bin/bash
 
-counter=0
-startdir="$(pwd)"
-tcdir=""
-srcdir=""
-iso="additional"
-temp="../$iso"
-worksrc="worksrc"
-jqfile=""
-nosvn=false
-ver="7"
+STARTDIR="$(pwd)"
+SRCDIR=""
+ISO="additional"
+TEMP="../$ISO"
+WORKSRC="worksrc"
+JQ_FILE=""
+NOSVN=false
 
 [ -f "${iso}.iso" ] && rm "${iso}.iso"
 
 while [ ! -z "$1" ]; do
     case $1 in
-        "nosvn"|"-nosvn"|"/nosvn"|"NOSVN"|"-NOSVN"|"/NOSVN")
-            nosvn=true
+        "--n"|"--N")
+            NOSVN=true
             echo "Warning! SVN status will not checked."
             ;;
-        "6")
-            ver="6"
-            echo "TinyCore v6.x ISO will be used."
-            ;;
-        "help"|"-help"|"/help"|"-h"|"HELP"|"-HELP"|"/HELP"|"-H"|"/?")
+        "--h"|"--H")
             echo "Options:"
-            echo -e "\t6\t:\tuse TinyCore 6.x version."
-            echo -e "\tnosvn\t:\tuse unversioned copy of sources."
-            echo -e "\thelp\t:\tthis help."
+            echo -e "\t--n : use unversioned copy of sources."
+            echo -e "\t--h : this help."
             exit
             ;;
         *)
-            echo "Unknown parameter "$1""
-            exit
+            fnError "Unknown parameter \"$1\""
             ;;
     esac
-
     shift      
 done
 
-findinc() {
-# $1 - this folder will be scanned
-# $2 - this folder has been scanned previously and will be skipped
-	local tcdir=""
-	for i in "$1"/*; do
-		if [ -d "$i" -a ! -L "$i" -a "$i" != "$2" ]
-		then
-			if [ -d "$i/$ver.x/iso" -a -d "$i/$ver.x/tcz" ]
-			then 
-				tcdir="$i/$ver.x"
-				echo "$tcdir"
-			else
-				findinc "$i"
-			fi
-		fi
-	done
-}
-
-finddec() {
-	local tcdir=$(findinc "$1" "$2")
-	let "counter = counter + 1"
-	
-	[ "$counter" -eq 8 ] && exit 1
-	
-	if [ -z "$tcdir" ]
-	then
-		local parentdir="$(dirname "$1")"
-		finddec "$parentdir" "$1"
-	else
-		echo "$tcdir"
-	fi
-}
-
-error() {
-	[ -z "$1" ] || echo "$1"
+fnError() {
+	[ -z "$1" ] || printf "ERROR: $1"
 	exit 1
 }
 
-svnerror() {
-	error "SVN exporting error!"
+fnSVNError() {
+	fnError "SVN exporting error!"
 }
 
-delfolder() {
+fnDelFolder() {
 	[ -z "$1" ] && return
 	if [ -d "$1" ]
 	then
@@ -90,84 +47,105 @@ delfolder() {
 	fi
 }
 
-makeexport() {
+fnMakeExport() {
 	[ $# -ge 2 ] || return
 	[ -z "$3" ] && echo "> exporting $1" || echo "> exporting $3"
 	[ -z "$(svn status "$1" | grep "^M")" ] || echo ">> uncommited files exists!"
-	svn export "$1" "$2" > /dev/null || svnerror
+	svn export "$1" "$2" > /dev/null || fnSVNError
 }
 
-makecopy() {
+fnMakeCopy() {
 	[ $# -ge 2 ] || return
 	[ -z "$3" ] && echo "> copying $1" || echo "> copying $3"
     [ -z "$4" ] && cp -R "$1" "$2" > /dev/null
 }
 
-exportsvn() {
+fnExportSVN() {
 	VERFILE="${1}/img/root/hasq/build.txt"
 	[ -z "$1" ] && return
-	if [[ "$nosvn" = false ]]
-    then
-        makeexport src "${1}/src" "src folder"
-        makeexport srcu "${1}/srcu" "srcu folder"
-        makeexport img "${1}/img" "img folder"
-        svnversion > "$VERFILE"
-        mkdir -p "${1}/ext/jq"
-        makeexport "ext/jq/$jqfile" "${1}/ext/jq/$jqfile" "external libraries"
+    fnMakeExport src "$1/src" "src folder"
+    fnMakeExport srcu "$1/srcu" "srcu folder"
+    fnMakeExport img "$1/img" "img folder"
+    svnversion > "$VERFILE"
+    mkdir -p "$1/ext/jq"
+    fnMakeExport "ext/jq/$JQ_FILE" "$1/ext/jq/$JQ_FILE" "external libraries"
 # TODO: find ":" in rev num
-        grep "M" "$VERFILE" && echo ">> revision: $(cat "$VERFILE")"
-    else
-        makecopy src "${1}/src" "src folder"
-        makecopy srcu "${1}/srcu" "srcu folder"
-        makecopy img "${1}/img" "img folder"
-        svnversion > "$VERFILE"
-        mkdir -p "${1}/ext/jq"
-        makecopy "ext/jq/$jqfile" "${1}/ext/jq/$jqfile" "external libraries"
-    fi
+    grep "M" "$VERFILE" && echo ">> revision: $(cat "$VERFILE")"
 }
 
-delfolder "$temp"
+fnCopyLocal() {
+	VERFILE="${1}/img/root/hasq/build.txt"
+	[ -z "$1" ] && return
+    fnMakeCopy src "$1/src" "src folder"
+    fnMakeCopy srcu "$1/srcu" "srcu folder"
+    fnMakeCopy img "$1/img" "img folder"
+    svnversion > "$VERFILE"
+    mkdir -p "$1/ext/jq"
+    fnMakeCopy "ext/jq/$JQ_FILE" "$1/ext/jq/$JQ_FILE" "external libraries"
+}
 
-echo "> searching for TinyCore folder..."
+fnGetDir() {
+    echo "$(echo $1 | head -c 2)/$1"
+}
 
-tcdir=$(finddec "$startdir")
-
-[ -z "$tcdir" ] && error "TinyCore folder not found!"
-if [[ "$nosvn" = false ]]
-then
-    [ -z "$(svn status "$tcdir/iso" 2>&1 | grep "was not found.")" -a -z "$(svn status "$tcdir/tcz" 2>&1 | grep "was not found.")" ] || echo ">>$tcdir is not a working copy of TinyCore."
-    [ -z "$(svn status "$tcdir" | grep "^M")" ] || error ">>$tcdir - folder have uncommited changes, please commit it before."
-    [ -z "$(svn status "$tcdir" | grep "^!")" ] || error ">>$tcdir - folder have missing files, please update it before."
-fi
+fnGetMD5() {
+    echo "$(md5sum $1 | head -c 32)"
+}
 
 
-[[ "$nosvn" = false ]] && echo "> exporting to $temp..." || echo "> copying to $temp..."
+fnDelFolder "$TEMP"
 
-if [[ "$nosvn" = false ]]
-then
-    makeexport "$tcdir"/tcz "$temp" > /dev/null || svnerror
-else
-    makecopy "$tcdir"/tcz "$temp" > /dev/null   
-fi
+VER="7.1"
+TC="Core-$VER.iso"
+TC_MD5="3d8c6fd16c5812b4b94b985f37230316"
+TCZ="tcz-$VER.fcl"
+TCZ_MD5="652c5413ccef9b631416848fe3e0d9cb"
+REPO="https://github.com/hasq/bin/raw/master/md5/"
+ISO_LNK="$REPO$(fnGetDir "$TC_MD5")/$TC"
+TCZ_LNK="$REPO$(fnGetDir "$TCZ_MD5")/$TCZ"
+ERR_ISO="Required file \"$TC\" is missing or corrupt.\nDownload it using the links:\n$ISO_LNK\n"
+ERR_TCZ="Required file \"$TCZ\" is missing or corrupt.\nDownload it using the links:\n$TCZ_LNK\n"
 
-cp "$tcdir"/iso/* "${temp}/"
+[ ! -f "$TC" ] && fnError "$ERR_ISO"
+[ ! -f "$TCZ" ] && fnError "$ERR_TCZ"
+[ $(fnGetMD5 "$TC") == "$TC_MD5" ] && printf "$TC\t - OK\n" || fnError "$ERR_ISO" 
+[ $(fnGetMD5 "$TCZ") == "$TCZ_MD5" ] && printf "$TCZ\t - OK\n"|| fnError "$ERR_TCZ" 
 
-[ -d ".." -a -d "../src" -a -d "../srcu" -a -d "../ext/jq" ] && srcdir=".."
-[ -z "$srcdir" ] && error "Source folder incorrect. Run this script from the proper folder."
-jqfile="$(basename $(find ../ext/jq/ -name jquery-ui-*.bz2))"
-cd "$srcdir" || exit
-delfolder "$worksrc"
 
-TARGETDIR="${startdir}/${temp}/${worksrc}"
+[[ "$NOSVN" = false ]] && echo "> exporting to $TEMP..." || echo "> copying to $TEMP..."
+
+#mkdir -p "$TEMP/tcz"
+mkdir -p "$TEMP" || fnError "Cannot create temp dir"
+#cp "$TCZ" "$TEMP/tcz"
+cp "$TCZ" "$TEMP"
+cd "$TEMP"
+fcl3 extr "$TCZ"
+rm -rf "$TCZ"
+cd "$STARTDIR"
+cp "$TC" "$TEMP/"
+
+[ -d ".." -a -d "../src" -a -d "../srcu" -a -d "../ext/jq" ] && SRCDIR=".."
+[ -z "$SRCDIR" ] && fnError "Source folder incorrect. Run this script from the proper folder."
+JQ_FILE="$(basename $(find ../ext/jq/ -name jquery-ui-*.bz2))"
+cd "$SRCDIR" || exit
+
+fnDelFolder "$WORKSRC"
+
+TARGETDIR="$STARTDIR/$TEMP/$WORKSRC"
 
 [ -d "$TARGETDIR" ] || mkdir -p "$TARGETDIR"
 
-exportsvn "$TARGETDIR"
+if [[ "$NOSVN" = false ]]
+then
+    fnExportSVN "$TARGETDIR"
+else
+    fnCopyLocal "$TARGETDIR"
+fi
 
-cd "$startdir"
-[ -d "$temp" -a -d "${temp}/$worksrc" ] || exit
-which genisoimage > /dev/null 2>&1 || error "! please install genisoimage"
+cd "$STARTDIR"
+[ -d "$TEMP" -a -d "$TEMP/$WORKSRC" ] || exit
+which genisoimage > /dev/null 2>&1 || fnError "! please install genisoimage"
 echo "> making iso"
-genisoimage -J -D -o "${iso}.iso" "$temp" > /dev/null 2>&1
-[ $? -eq 0 -a -f "${iso}.iso" ] && echo ">> OK!"
-delfolder "$temp"
+genisoimage -J -D -o "$ISO.iso" "$TEMP" > /dev/null 2>&1
+[ $? -eq 0 -a -f "$ISO.iso" ] && echo ">> OK!"
+fnDelFolder "$TEMP"
