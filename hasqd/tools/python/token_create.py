f85a7bc31a2709fb51e7b4b79ba802556f8df0a8
underr <underr-ua@users.noreply.github.com>
2016-09-14 21:06:37 +0300

    py update

--- hasqd/tools/python/token_create.py
+++ hasqd/tools/python/token_create.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# token_create (name|@file) [password]
+# token_create (name|@file) [password|-]
  
 import sys
 import urllib2
@@ -11,7 +11,7 @@ import ts_msg
 
 argv_len = len(sys.argv)
 
-if argv_len == len(sys.argv) < 2 or argv_len == len(sys.argv) > 3:
+if argv_len == len(sys.argv) < 3:
     print("{} {} {} {}". format(ts_msg.get_msg("hlp_usg"), sys.argv[0],
         ts_msg.get_msg("hlp_tkn"), ts_msg.get_msg("hlp_pwd")))
     quit()
@@ -24,33 +24,40 @@ else:
     file_name=sys.argv[1][1:]
 
     if not ts_lib.is_file(file_name):
-        print("{} {}: {}".format(ts_msg.get_msg("err_msg"), ts_msg.get_msg("fil_mng"), file_name))
+        print("{} {}: {}".format(ts_msg.get_msg("err_msg"),
+            ts_msg.get_msg("fil_mng"), file_name))
         quit()
   
     try:
         f = open(file_name, 'rb')
     except:
-        print("{} {}: {}".format(ts_msg.get_msg("err_msg"), ts_msg.get_msg("fil_err"), file_name))
+        print("{} {}: {}".format(ts_msg.get_msg("err_msg"),
+            ts_msg.get_msg("fil_err"), file_name))
         quit()
     else:
         token_name = ts_lib.get_tok_hash(f.read(), ts_cnf.HASH_NAME)
         print(token_name)
         f.close()
         
-quit()
-    
-master_key = getpass.getpass("{}:".format(ts_msg.get_msg("key_etr")))
+master_key = (sys.argv[2] 
+        if sys.argv[2] != "-" 
+        else getpass.getpass("{}:".format(ts_msg.get_msg("key_etr"))))
 
 if master_key.lower() == "":
+    print("{} {}".format(ts_msg.get_msg("err_msg"), ts_msg.get_msg("key_err")))
     quit()
 
-print("Token hash: {}".format(ts_lib.get_tok_hash(token_name, ts_cnf.HASH_NAME)))
-print("Master key: {}".format(master_key))
+#print("Token hash: {}".format(ts_lib.get_tok_hash(token_name, ts_cnf.HASH_NAME)))
+#print("Master key: {}".format(master_key))
 
-r = ts_lib.get_record(0, ts_lib.get_tok_hash(token_name, ts_cnf.HASH_NAME), master_key, ts_cnf.MAGIC, ts_cnf.HASH_NAME)
-d = token_name if token_name != ts_lib.get_tok_hash(token_name, ts_cnf.HASH_NAME) else ""
+r = ts_lib.get_record(0, ts_lib.get_tok_hash(token_name, ts_cnf.HASH_NAME),
+        master_key, ts_cnf.MAGIC, ts_cnf.HASH_NAME)
+d = (token_name 
+        if token_name != ts_lib.get_tok_hash(token_name, ts_cnf.HASH_NAME)
+        else "")
 
-http_rqst = ts_lib.get_spaced_conc(CMD, "*", ts_cnf.DB, str(r.get("n")), r.get("s"), r.get("k"), r.get("g"), r.get("o"), d)
+http_rqst = ts_lib.get_spaced_conc(CMD, "*", ts_cnf.DB, str(r.get("n")),
+                        r.get("s"), r.get("k"), r.get("g"), r.get("o"), d)
 
 print(http_rqst)
 
@@ -59,4 +66,4 @@ try:
 except:
     print ("{} {} {}:{}".format(ts_msg.get_msg("err_msg"), ts_msg.get_msg("fil_mng"), ts_cnf.HOST, ts_cnf.PORT))
 else:
-    print("{} {}".format(ts_msg.get_msg("srv_rpl"), http_resp))
+    print("{}: {}".format(ts_msg.get_msg("srv_rpl"), http_resp))
--- hasqd/tools/python/ts_msg.py
+++ hasqd/tools/python/ts_msg.py
@@ -6,6 +6,7 @@ def get_msg(data):
         "fil_err" : "File read error",
         "fil_mng" : "File is empty or missing",
         "key_etr" : "Enter master key",
+        "key_err" : "Master key is empty.",
         "hlp_usg" : "Usage: python",
         "hlp_tkn" : "(token name|@file)",
         "hlp_pwd" : "[password|-]"        
