8d477127839bcdc8d03afd94c1c864a5341ed2e2
underr <underr-ua@users.noreply.github.com>
2016-09-26 14:34:31 +0300

    sync tokenswap and hashnote

--- hasqd/tests/manual/syncdb/syncdb.sh
+++ hasqd/tests/manual/syncdb/syncdb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -rf ts_list.* ts_slcs ts_idxs hn_list.* syncdb.log hn_slcs hn_idxs
+rm -rf ts_list.* syncdb.log index slice ts_slcs ts_idxs hn_list.* hn_slcs hn_idxs
 
 INP=$(basename $0)
 INP=${INP%.*} 
@@ -15,16 +15,18 @@ error() {
 
 [ ! -f "$HIDEE" ] && error "Please compile hidee.cpp first!"
 [ ! -f "myname" ] && "$HIDEE"
-[ -f skc.key.e -a -f "$HIDEE" ] && "$HIDEE" skc.key.e || error "SKC-key file is missing!"
+[ -f "skc.key.e" ] && "$HIDEE" skc.key.e || error "SKC-key file is missing!"
 
 SKCKEY="$(cat skc.key)"
 
 rm -f skc.key
 
 CMD_ZDB0="erasedisk"
-CMD_ZDB1="create smd.db smd SMD 1 [] 1 0"
+CMD_ZDB1="create smd.db smd SMD 1 [] 100 0"
 CMD_HASQD="-lqnxc dprn=1 dsvt=1 dagt=1 dwkr=1 tcp_port=14000 skckey=$SKCKEY s=$INP.inp"
 
 "$P/zdb" "$CMD_ZDB0" "$CMD_ZDB1"
 #echo "$CMD_HASQD"
 "$P/hasqd" $CMD_HASQD | grep -v "noterecv\|conupdate"
+
+rm -rf ts_list.* index slice ts_slcs ts_idxs hn_list.* hn_slcs hn_idxs
