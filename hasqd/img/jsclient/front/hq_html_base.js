d17373dae25def54fc6e0df71d8e1c97a11811f2
ab <underr.ua@gmail.com>
2018-02-23 12:29:02 +0200

    width

--- hasqd/img/jsclient/front/hq_html_base.js
+++ hasqd/img/jsclient/front/hq_html_base.js
@@ -129,7 +129,7 @@ function widGetHTMLLogArea() {
                     }
                     r += '</table>\n';
                 }
-                r += '<td class="td-subscribe" style="width: 160px;"/>\n';
+                r += '<td class="td-subscribe" style="width: 230px;"/>\n';
                 r += '<a href="http://hasq.org">Hasq Technology&nbsp;\u00A9&nbsp;2016</a>\n';
             }
             r += '</tr>\n';
