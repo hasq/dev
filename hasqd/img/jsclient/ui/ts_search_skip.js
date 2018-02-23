058d4ff5e87fdb35d4a32181568ad8efa948c9f8
U-ZONK\oleg <you@example.com>
2018-02-23 13:16:17 +0400

    z

---
+++ hasqd/img/jsclient/ui/ts_search_skip.js
@@ -0,0 +1,9 @@
+// Hasq Technology Pty Ltd (C) 2013-2018
+
+// this variable defines ranges to skip when searching
+// could be customized for each server/database
+// to improve performance
+
+'use strict';
+
+var gSkipRanges = [ [ 20180222, 20160526 ], [ 20160524, 20150101] ];
